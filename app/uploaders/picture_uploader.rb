9dcec52d64fbdb866101c306c4866eec6b2f3674
geraudmathe <mail@geraudmathe.com>
2015-12-07 13:40:16 +0000

    added tagging lib

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -16,6 +16,13 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  after :cache, :tag_image
+
+  def tag_image(file)
+    puts file.inspect
+    ImageTagging.new(file)
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
---
+++ lib/image_tagging.rb
@@ -0,0 +1,14 @@
+class ImageTagging
+  def initialize file
+    url = URI("http://api.imagga.com/v1/tagging?url=http%3A%2F%2Fplayground.imagga.com%2Fstatic%2Fimg%2Fexample_photo.jpg&version=2")
+
+    http = Net::HTTP.new(url.host, url.port)
+
+    request = Net::HTTP::Get.new(url)
+    request["accept"] = 'application/json'
+    request["authorization"] = 'Basic YWNjXzUwYWZiODE0NzU2YWQ1MTpjMzA2OThiYTczMGE3NGMxY2Q0M2Y3ZTM4MjU5OTQzYQ=='
+
+    response = http.request(request)
+    p JSON.parse(response.read_body)["results"][0]["tags"].map{ |t| t["tag"] }
+  end
+end
